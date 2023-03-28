10428c3a7881e0477aa63ac62e18677e887126f2
mrussotti <matthewlax24@yahoo.com>
2023-03-28 18:35:06 -0400

    added css. cool animation for nav bar

--- src/pages/Intermediate.js
+++ src/pages/Intermediate.js
@@ -19,35 +19,16 @@ export default function Intermediate() {
   return (
     <>
       <h1 className={styles.pageTitle}>Intermediate</h1>
-      <div className={styles.rectangleCardContainer}>
-        <div className={styles.rectangleCard}>
           <RectangleCard heading="Intro" text={text4} />
-        </div>
-        <div className={styles.rectangleCard}>
           <RectangleCard heading="Stimulus to fatigue ratio" text={text1} />
-        </div>
-        <div className={styles.rectangleCard}>
           <RectangleCard heading="Advanced vocab Vocab" text={"MEV, MAV, Mesocycle. etc"} />
-        </div>
-        <div className={styles.rectangleCard}>
           <RectangleCard heading="Advanced nutrition" text={text2} />
-        </div>
-        <div className={styles.rectangleCard}>
           <RectangleCard heading="pre workout breakdown" text={"what are ingredients and what they do, red flags to look out for"} />
-        </div>
-        <div className={styles.rectangleCard}>
           <RectangleCard heading="Different kinds of sets" text={text3} />
-        </div>
-        <div className={styles.rectangleCard}>
           <RectangleCard heading="Need for custom workout plans and how to make one" text={text5} />
-        </div>
-        <div className={styles.rectangleCard}>
         <CalorieCalculator/>
-        </div>
-        <div className={styles.rectangleCard}>
         <MuscleVolumeCalculator/>
-        </div>
-      </div>
+
     </>
   );
 }
