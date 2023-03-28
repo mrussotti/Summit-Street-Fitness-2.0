75b237b72c262157353ff99eaf975afc4c88afb5
mrussotti <matthewlax24@yahoo.com>
2023-03-28 16:05:23 -0400

    added some styles

--- src/pages/Beginner.js
+++ src/pages/Beginner.js
@@ -67,58 +67,36 @@ export default function Beginner() {
   return (
     <>
       <h1 className={styles.pageTitle}>Beginner</h1>
-      <div className={styles.rectangleCard}>
         <MarkdownCard heading='Outline Your Goals' markdownContent={Outline} />
-      </div>
-      <div className={styles.rectangleCard}>
         {!recommendation && (
           <RecommendationForm onSubmit={handleFormSubmit} />
         )}
         {recommendation && (
-          <div className={styles.rectangleCardContainer}>
+          <>
             {recommendation === 'Bulk' && (
                 <MarkdownCard heading='Bulking for Beginner Lifters' markdownContent={content.BulkDiet} />
             )}
 
             {recommendation === 'Cut' && (
-              
                 <MarkdownCard heading='Cutting for Beginner Lifters' markdownContent={content.CutDiet} />
-              
             )}
 
             {recommendation === 'Recomp' && (
                 <MarkdownCard heading='Body Recomposition for Beginner Lifters' markdownContent={content.RecompDiet} />
             )}
-            
-            {/* Add more conditional rendering for other Markdown cards as needed */}
-                      </div>
+           </>
                     )}
-                  </div>
-            
-                  <div className={styles.rectangleCard}>
                     <MarkdownCard heading='Training' markdownContent={content.trainingContent} />
-                  </div>
-                  <div className={styles.rectangleCard}>
                     <MarkdownCard heading='Recovery' markdownContent={content.recoveryContent} />
-                  </div>
-                  <div className={styles.rectangleCard}>
                     <MarkdownCard heading='Supplements To Start With' markdownContent={content.supplementsContent} />
-                  </div>
-                  <div className={styles.rectangleCard}>
                     <MarkdownCard heading='What Gym Gear Do You Need?' markdownContent={content.gearContent} />
-                  </div>
-                  <div className={styles.rectangleCard}>
                     <MarkdownCard heading='Gym Etiquette' markdownContent={content.etiquetteContent} />
-                  </div>
-                  <div className={styles.rectangleCard}>
                     <MarkdownCard heading='Myth Busting' markdownContent={Myths} />
-                  </div>
-                  <div className={styles.rectangleCard}>
                     <MarkdownCard
                       heading="How To Know When You Are An Intermediate Lifter"
                       markdownContent={content.progressContent}
                     />
-                  </div>
+                  
                 </>
               );
             }
